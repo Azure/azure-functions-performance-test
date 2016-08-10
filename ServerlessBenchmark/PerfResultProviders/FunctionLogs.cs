d552ab0c3f6b3f24aabaaf3a76f2659bc28bf5f9
Krzysztof Knigawka <krzysztof.knigawka@hotmail.com>
2016-08-10 13:27:44 -0700

    Add timeout on waiting for azure function logs.

--- ServerlessBenchmark/PerfResultProviders/FunctionLogs.cs
+++ ServerlessBenchmark/PerfResultProviders/FunctionLogs.cs
@@ -17,60 +17,17 @@ namespace ServerlessBenchmark.PerfResultProviders
         private static List<AzureFunctionLogs> _azurefunctionLogs; 
         public static List<AzureFunctionLogs> GetAzureFunctionLogs(string functionName, DateTime? startTime, DateTime? endTime, bool update = false, int expectedExecutionCount = 0)
         {
-            Console.WriteLine("Getting Azure Function logs from Azure Storage Tables..");
-            var connectionString = ConfigurationManager.AppSettings["AzureStorageConnectionString"];
-            if (!string.IsNullOrEmpty(connectionString))
-            {
-                var storageAccount = CloudStorageAccount.Parse(connectionString);
-                var tableClient = storageAccount.CreateCloudTableClient();
-                var table = tableClient.GetTableReference("AzureFunctionsLogTable");
-                var query = new TableQuery<AzureFunctionLogs>();
-                int size = 0;
-                do
-                {
-                    _azurefunctionLogs = table.ExecuteQuery(query).Where(log => !string.IsNullOrEmpty(log.FunctionName) &&
-                        log.FunctionName.Equals(functionName, StringComparison.CurrentCultureIgnoreCase) && !string.IsNullOrEmpty(log.ContainerName)).ToList();
-                    size = _azurefunctionLogs.Count();
-                    Console.WriteLine("Log count - " + size);
-                    if (expectedExecutionCount == 0)
-                    {
-                        break;
-                    }
-                    Thread.Sleep(1000);
-                } while (size < expectedExecutionCount);
-            }
-            return _azurefunctionLogs;
+            return GetAzureFunctionLogsInternal(functionName, startTime, endTime, update, expectedExecutionCount);
         }
 
         public static List<AzureFunctionLogs> GetAzureFunctionLogs(DateTime? startTime, DateTime? endTime, bool update = false, int expectedExecutionCount = 0)
         {
-            Console.WriteLine("Get Azure Function logs from Azure Storage Tables..");
-            var connectionString = ConfigurationManager.AppSettings["AzureStorageConnectionString"];
-            if (!string.IsNullOrEmpty(connectionString))
-            {
-                var storageAccount = CloudStorageAccount.Parse(connectionString);
-                var tableClient = storageAccount.CreateCloudTableClient();
-                var table = tableClient.GetTableReference("AzureFunctionsLogTable");
-                var query = new TableQuery<AzureFunctionLogs>();
-                int size = 0;
-                do
-                {
-                    _azurefunctionLogs = table.ExecuteQuery(query).ToList();
-                    size = _azurefunctionLogs.Count();
-                    Console.WriteLine("Log count - " + size);
-                    Thread.Sleep(1000);
-                } while (size < expectedExecutionCount);
-            }
-            return _azurefunctionLogs;
+            return GetAzureFunctionLogsInternal(string.Empty, startTime, endTime, update, expectedExecutionCount);
         }
 
         public static List<AzureFunctionLogs> GetAzureFunctionLogs(string functionName, bool allLogs = false)
         {
-            if (string.IsNullOrEmpty(functionName))
-            {
-                return GetAzureFunctionLogs(null, null);
-            }
-            return GetAzureFunctionLogs(functionName, null, null);
+            return GetAzureFunctionLogsInternal(functionName, null, null);
         }
 
         public static bool RemoveAllCLoudWatchLogs(string functionName)
@@ -84,6 +41,63 @@ namespace ServerlessBenchmark.PerfResultProviders
                 isEmpty = GetAllLogStreams(functionName, cwClient).Count == 0;
             }
             return isEmpty;
+        }
+
+        private static List<AzureFunctionLogs> GetAzureFunctionLogsInternal(string functionName, DateTime? startTime, DateTime? endTime, bool update = false, int expectedExecutionCount = 0, int waitForAllLogsTimeoutInMinutes = 5)
+        {
+            Console.WriteLine("Getting Azure Function logs from Azure Storage Tables..");
+            var connectionString = ConfigurationManager.AppSettings["AzureStorageConnectionString"];
+
+            if (!string.IsNullOrEmpty(connectionString))
+            {
+                var storageAccount = CloudStorageAccount.Parse(connectionString);
+                var tableClient = storageAccount.CreateCloudTableClient();
+                var table = tableClient.GetTableReference("AzureFunctionsLogTable");
+                var query = new TableQuery<AzureFunctionLogs>();
+                int size = 0;
+                var latestNewLog = DateTime.UtcNow;
+                var lastSize = 0;
+
+                do
+                {
+                    _azurefunctionLogs = table.ExecuteQuery(query).Where(log => string.IsNullOrEmpty(functionName) || (!string.IsNullOrEmpty(log.FunctionName) &&
+                        log.FunctionName.Equals(functionName, StringComparison.CurrentCultureIgnoreCase) && !string.IsNullOrEmpty(log.ContainerName))).ToList();
+                    size = _azurefunctionLogs.Count();
+
+                    if (lastSize != size)
+                    {
+                        lastSize = size;
+                        latestNewLog = DateTime.UtcNow;
+                    }
+                    else
+                    {
+                        var secondsSinceLastNewLog = (DateTime.UtcNow - latestNewLog).TotalSeconds;
+                        var secondsStillToWait = 60*waitForAllLogsTimeoutInMinutes - secondsSinceLastNewLog;
+                        Console.WriteLine(
+                            "No new log for {0} seconds. Waiting another {1}s to finish.", 
+                            secondsSinceLastNewLog,
+                            secondsStillToWait
+                            );
+                    }
+
+                    if (expectedExecutionCount == 0)
+                    {
+                        break;
+                    }
+
+                    Console.WriteLine("Log count {0} expected {1}", size, expectedExecutionCount);
+
+                    if ((DateTime.UtcNow - latestNewLog).TotalMinutes >= waitForAllLogsTimeoutInMinutes)
+                    {
+                        Console.WriteLine("Not all result logs have been found! No new logs appeared in last {0} minutes. Finishing wait to present results.", waitForAllLogsTimeoutInMinutes);
+                        break;
+                    }
+                    
+                    Thread.Sleep(1000);
+                } while (size < expectedExecutionCount);
+            }
+
+            return _azurefunctionLogs;
         }
 
         private static List<LogStream> GetAllLogStreams(string functionName, AmazonCloudWatchLogsClient cwClient)

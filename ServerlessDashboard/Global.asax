6ee96e92e52a7b757642cc9170868384f8d1b6f0
Krzysztof Knigawka <krzysztof.knigawka@hotmail.com>
2016-08-26 11:44:48 -0700

    Add serverless dashoard initial project.

---
+++ ServerlessDashboard/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ServerlessDashboard.MvcApplication" Language="C#" %>
---
+++ ServerlessDashboard/Startup.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ServerlessDashboard.Startup))]
+namespace ServerlessDashboard
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+        }
+    }
+}

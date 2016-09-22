fd2f923055f50fdc61b9caa8ff3dd1bc4c5d6884
Paul Batum <paul.batum@gmail.com>
2016-09-22 15:22:48 -0700

    Node queue test script updates - add outputs to ensure compatibility with QueueTest - remove 1000 log entry step from low usage scenario

--- SampleUsages/TestSamples/Queue-NodeJs-HighMemory/index.js
+++ SampleUsages/TestSamples/Queue-NodeJs-HighMemory/index.js
@@ -4,15 +4,16 @@ module.exports = function (context, size) {
     var array = [];
     var seed = size;
 
-    for(var i = 0; i < size; i++){
+    for (var i = 0; i < size; i++) {
         var x = Math.random(seed) * size;
         array.push(x);
     }
 
-    for(var j = 1; j < size; j++){
+    for (var j = 1; j < size; j++) {
         var y = Math.random(seed) * size;
         array[j] = array[j - 1] + y;
     }
-    
+
+    context.bindings.output = size;
     context.done();
 }

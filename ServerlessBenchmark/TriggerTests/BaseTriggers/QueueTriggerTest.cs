9c5c63b40c3c6aa036fd91b65fbe66f98d9537f6
Paul Batum <paul.batum@gmail.com>
2016-10-20 16:51:21 -0700

    Fixed a pretty annoying EPS / warmup time swap bug

--- ServerlessBenchmark/TriggerTests/BaseTriggers/QueueTriggerTest.cs
+++ ServerlessBenchmark/TriggerTests/BaseTriggers/QueueTriggerTest.cs
@@ -19,7 +19,7 @@ namespace ServerlessBenchmark.TriggerTests.BaseTriggers
         protected string SourceQueue { get; set; }
         protected string TargetQueue { get; set; }
 
-        protected QueueTriggerTest(string functionName, int eps, int warmUpTimeInMinutes, string[] messages, string sourceQueue, string targetQueue):base(functionName, warmUpTimeInMinutes, eps, messages)
+        protected QueueTriggerTest(string functionName, int eps, int warmUpTimeInMinutes, string[] messages, string sourceQueue, string targetQueue):base(functionName, eps, warmUpTimeInMinutes, messages)
         {
             SourceQueue = sourceQueue;
             TargetQueue = targetQueue;
